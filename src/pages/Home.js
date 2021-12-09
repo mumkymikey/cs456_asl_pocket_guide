5cf016ee749d4da2beaf9bf7d2e55dd380982736
mumkymikey <michael.day0621@gmail.com>
2021-12-08 18:05:46 -0600

    add content to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 function Home() {
   return(
     <div>
-      <h1>Welcome to the ASL Pocket Guide!</h1>
-      <h2>Navigate the site using the sidebar</h2>
+      <h1 style={{ textAlign: 'center' }}>Welcome to the ASL Pocket Guide!</h1><br />
+      <h2>
+        American Sign Language (ASL) is the primary sign language for Deaf communities in the US.
+        And like with any language, there's a steep learning curve in becoming fluent in ASL.
+        That's what this pocket guide exists for!
+      </h2>
+      <h2>
+        The ASL Pocket Guide provides a way for users to quickly find out how to sign each letter,
+        The guide also provides the user with resources to learning ASL, as well as with resources
+        regarding hearing loss in general.
+      </h2>
+      <h2>
+        Navigate the pocket guide by using the sidebar navigation.
+      </h2>
     </div>
   )
 }
